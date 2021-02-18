14f46c8f355b0d07693a0e3e5fd13fbcc296f722
Abhishek Kumar <70133941+abhishek3stp@users.noreply.github.com>
2021-02-19 00:09:27 +0530

    security

--- CustomerDB/settings.py
+++ CustomerDB/settings.py
@@ -139,5 +139,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'abhishek2stp@gmail.com'
-EMAIL_HOST_PASSWORD = 'Abhi@1999'
+EMAIL_HOST_USER = '***********'
+EMAIL_HOST_PASSWORD = '*********'
